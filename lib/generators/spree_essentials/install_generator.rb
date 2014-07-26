d04ae3620f4a534e1bd732cf492372e81ceeb68d
shenwei235 <shenwei235@gmail.com>
2014-07-26 14:05:38 +0800

    Update install_generator.rb

--- lib/generators/spree_essentials/install_generator.rb
+++ lib/generators/spree_essentials/install_generator.rb
@@ -4,17 +4,17 @@ module SpreeEssentials
       
       desc "Installs required migrations for spree_essentials"
       
-      class_option :add_stylesheets, :type => :boolean, :default => true, :banner => 'Append spree_essentials to admin/all.css'
-      class_option :add_javascripts, :type => :boolean, :default => true, :banner => 'Append spree_essentials to admin/all.js'
+      class_option :add_stylesheets, :type => :boolean, :default => true, :banner => 'Append spree_essentials to spree/backend.css'
+      class_option :add_javascripts, :type => :boolean, :default => true, :banner => 'Append spree_essentials to spree/backend.js'
       
       def append_stylesheets
         return unless options[:add_stylesheets]
-        gsub_file "app/assets/stylesheets/admin/all.css", "*/", "*= require admin/spree_essentials\n*/"  
+        gsub_file "app/assets/stylesheets/spree/backend.css", "*/", "*= require admin/spree_essentials\n*/"  
       end
       
       def append_javascripts
         return unless options[:add_javascripts]
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_essentials"
+        append_file "app/assets/javascripts/spree/backend.js", "//= require admin/spree_essentials"
       end
 
     end
